c1e5930d7f78366a50da3b94b6b4e5ccc98c004c
iVerkhov <vrkhv@iCloud.com>
2023-09-08 21:16:00 +0300

    Initial Commit

---
+++ HabbitsTracker/HabbitsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitsTrackerApp.swift
+//  HabbitsTracker
+//
+//  Created by Игорь Верхов on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabbitsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
